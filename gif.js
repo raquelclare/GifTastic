46e51e79672bbbcaa72ea83e14c51f81ba1aaf59
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-05-14 11:58:23 -0500

    adding beginnings of my code structue

---
+++ gif.js
@@ -0,0 +1,20 @@
+// Creat an array of strings, each one related to a topic that interests me
+var topics = [];
+
+// Take the topics in this array and create buttons in my HTML
+// Use a loop that appends a button for each string in the array into 
+// the dynamic buttons div
+
+//When the user clicks on a button, the page should grab 10 static, 
+// non-animated gif images from the GIPHY API and place them in the gifs div
+
+// UNDER every gif, display its rating--- what if I only want G and PG rated gifs
+// This data is provided by the GIPHY API
+
+//===== Do the above before moving on =====
+
+// Add a form to your page that takes the value from a user input box and adds it 
+// to your topics array
+
+// Lastly, make a function call that takes each topic in the array that remakes the
+// buttons on the page
